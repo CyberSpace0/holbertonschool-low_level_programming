c76bc0d7d9ac38bca0141f1ee19fe8651585c50a
Your Name <you@example.com>
2026-02-23 11:57:49 +0000

    fdasf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,10 +4,10 @@
 * Return
 */
 int main() {
-printf("Size of a char: %i byte(s)",sizeof(char));
-printf("Size of a char: %i byte(s)",sizeof(int));
-printf("Size of a char: %i byte(s)",sizeof(long int));
-printf("Size of a char: %i byte(s)",sizeof(long long int));
-printf("Size of a char: %i byte(s)",sizeof(float));
+printf("Size of a char: %lu byte(s)\n",sizeof(char));
+printf("Size of a int: %lu byte(s)\n",sizeof(int));
+printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n",sizeof(float));
 return (0);
 }
