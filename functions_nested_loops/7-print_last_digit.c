07106035ae84eb09b97c5abca560b20754e4d1ac
Your Name <you@example.com>
2026-02-25 11:54:53 +0000

    Holberton tasks

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _abs - entry point
+* * @c: the character to check
+* Return: always (0) success
+*/
+int print_last_digit(int c)
+{
+int last_digit = c % 10;
+return (last_digit);
+}
