06dd72fb7859980ad8582b1ec0a0bb35aa062e9d
Your Name <you@example.com>
2026-02-25 12:23:57 +0000

    Holberton tasks

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,10 @@
 int print_last_digit(int c)
 {
 int last_digit = c % 10;
-_putchar(last_digit);
+if (last_digit < 0)
+{
+last_digit = -last_digit;
+}
+_putchar('0'+last_digit);
 return (last_digit);
 }
