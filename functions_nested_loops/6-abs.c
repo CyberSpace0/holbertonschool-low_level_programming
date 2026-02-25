f332b01acfb41b424f668a9ed3f6c4f9e04018ba
Your Name <you@example.com>
2026-02-25 11:46:59 +0000

    Holberton tasks

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - entry point
+* * @c: the character to check
+* Return: always (0) success
+*/
+int _abs(int c)
+{
+if (c < 0)
+{
+return (-c);
+}
+else
+{
+return (c);
+}
+
+
+}
