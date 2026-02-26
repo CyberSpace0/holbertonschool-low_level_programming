5f639f5379e73ba1567d25613ed829c8ed1dfbbc
Your Name <you@example.com>
2026-02-26 09:34:04 +0000

    fad

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,7 @@
  * @i: current number
  * @n: starting number
  */
-void first(int i, int n)
-{
-    if (i == n)
-    {
-        printf("%i", i);
-        if (i != 98)
-            printf(",");
-    }
-    else if (i == 98)
-    {
-        printf(" %i", i);
-    }
-    else
-    {
-        printf(" %i,", i);
-    }
-}
+void first(int i, int n);
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
@@ -30,18 +14,45 @@ void first(int i, int n)
  */
 void print_to_98(int n)
 {
-    int i;
+int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-            first(i, n);
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-            first(i, n);
-    }
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+first(i, n);
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+first(i, n);
+}
+}
 
-    printf("\n");
+printf("\n");
+
+}
+
+void first(int i, int n)
+{
+if (i == n)
+{
+printf("%i", i);
+if (i != 98)
+{
+printf(",");
+}
+}
+
+else if (i == 98)
+{
+printf(" %i", i);
+}
+
+else
+{
+printf(" %i,", i);
+}
 }
