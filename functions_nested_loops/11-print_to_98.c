6b8373f3f4365f52d36ff5ed0530355ec19eebf9
Your Name <you@example.com>
2026-02-26 08:46:01 +0000

    daf

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,40 @@
 void print_to_98(int n)
 {
 int i;
-for (i = n; i <= 98; i++)
+
+void first()
 {
+
 if (i == 98)
 {
 printf("%i",i);
 }
+
 else if(i == n)
 {
 printf("%i,",i);
 }
+
 else
 {
 printf(" %i,",i);
 }
-
 }
+if (n <= 98)
+{
+for (i = n;i <= 98;i++)
+{
+first();
+}
+}
+else
+{
+for (i = n;i >= 98;i--)
+{
+first();
+}
+}
+
 printf("\n");
 
 }
