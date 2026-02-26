a4f6f581b78e69c510c23a8cb6cfb05ab9afc6ff
Your Name <you@example.com>
2026-02-26 08:17:27 +0000

    task

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* add - entry point
+* @c: int value
+* Return: always (0) success
+*/
+void print_to_98(int n)
+{
+int i;
+for (i = c; i <= 98; i++)
+{
+if (i == c)
+{
+printf("%i,",i);
+}
+else if(i == 98)
+{
+printf(" %i",i);
+}
+else
+{
+printf(" %i,",i);
+}
+
+}
+
+}
