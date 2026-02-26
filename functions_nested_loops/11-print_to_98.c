abd555702fea0ccfca196483e9e4389af575c345
Your Name <you@example.com>
2026-02-26 08:24:43 +0000

    fda

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@ void print_to_98(int n)
 int i;
 for (i = n; i <= 98; i++)
 {
-if (i == n)
-{
-printf("%i,",i);
-}
-else if(i == 98)
+if (i == 98)
 {
 printf(" %i",i);
+}
+else if(i == n)
+{
+printf("%i,",i);
 }
 else
 {
