d54d6934916989112b672d6f308c712f24e52b62
Your Name <you@example.com>
2026-03-02 14:25:39 +0300

    daf

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
-
+/**
+* _isdigit - Entry functino
+* @c:   int value
+* Return: always (0) Success
+*/
 int _isdigit(int c)
 {
 return ((isdigit(c) != 0) ? 1 : 0);
