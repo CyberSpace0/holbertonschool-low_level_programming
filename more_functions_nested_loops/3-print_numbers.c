5eae4d1212f369c998671ab5b8128aa28714edf9
Your Name <you@example.com>
2026-03-02 14:45:41 +0300

    daf

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_numbers - Entry functino
+* Return: always (0) Success
+*/
+void print_numbers(void)
+{
+for (int i=48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
