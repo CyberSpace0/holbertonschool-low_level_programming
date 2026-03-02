59676f2432091138447f27e80e50809e0c881e10
Your Name <you@example.com>
2026-03-02 15:01:40 +0300

    Holberton Task

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,15 @@ void print_most_numbers(void)
 int i;
 for (i = 48; i < 58; i++)
 {
-(i == 2 || i == 4) ? (void)0 : _putchar(i);
+if (i == 50 || i == 52)
+{
+(void)0;
+}
+else
+{
+_putchar(i);
+}
+
 }
 _putchar('\n');
 }
