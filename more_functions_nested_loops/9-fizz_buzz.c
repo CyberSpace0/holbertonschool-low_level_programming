99b0f344a0325ad467f188aff801c8b325c49c5c
Your Name <you@example.com>
2026-03-02 22:48:40 +0300

    Holberton task

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,19 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" %s ", "FizzBuzz");
+printf("%s ", "FizzBuzz");
 }
 else if(i % 5 == 0)
 {
-printf(" %s ", "Buzz");
+printf("%s ", "Buzz");
 }
 else if(i % 3 == 0)
 {
-printf(" %s ", "Fizz");
+printf("%s ", "Fizz");
 }
 else
 {
-printf(" %i ", i);
+printf("%i ", i);
 }
 }
 
