176174d116ae8e29b9eb4e30725e4843ec6b8b5c
Your Name <you@example.com>
2026-03-02 13:58:18 +0300

    holberton

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry functino
+* _isupper - Entry functino
 *@c int value
 * Return: always (0) Success
 */
 int _isupper(int c)
 {
-return (isupper(c));
+return (_isupper(c));
 }
