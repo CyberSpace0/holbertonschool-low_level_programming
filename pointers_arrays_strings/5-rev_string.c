dd682fffc9e5d131a746743e8ec5c060123f0609
Your Name <you@example.com>
2026-03-04 22:22:50 +0300

    Holberton task

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Entry function
+ * @str: char value from pointer
+ * Return: always int value
+ */
+void rev_string(char *s)
+{
+int len = 0;
+int i;
+while (str[len] != '\0')
+len++;
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
