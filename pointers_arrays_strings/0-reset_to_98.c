d21e537dc8657e4a4c375754dd09a01f89961f03
Your Name <you@example.com>
2026-03-04 13:38:24 +0300

    Holberton Task

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry function
+ * @n: int value from pointer
+ * Return: always int value
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,24 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - check the code
+ * main - check the code 
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int x;
-    int i[5] = {24,5,13,542,541};
-    for (x = 0; x < 5; x++)
-    {
-        printf(" %d => %p\n",i[x],&i[x]); 
-    }
-    printf(" %p \n",i);
-    printf(" %p \n",i+1);
-    printf(" %p \n",i+2);
-       
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
     return (0);
 }
-
