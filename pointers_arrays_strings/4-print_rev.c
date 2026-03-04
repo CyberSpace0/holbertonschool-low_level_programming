4e32a6b814a575294fd44d5499ad3f2e8020ba4b
Your Name <you@example.com>
2026-03-04 15:51:36 +0300

    Holberton Task

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *str)
 int len = 0;
 int i;
 while (str[len] != '\0')
-    len++;
+len++;
 for (i = len - 1; i >= 0; i--)
 {
-    _putchar(str[i]);
+_putchar(str[i]);
 }
 _putchar('\n');
 }
