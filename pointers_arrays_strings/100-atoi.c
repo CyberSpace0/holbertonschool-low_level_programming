99fd36b3f8e4016f887a1cf39b39a25a85ada297
Your Name <you@example.com>
2026-03-05 14:19:05 +0300

    Holberton Task

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
+#include <limits.h>
 /**
- * _atoi - convert a string to an integer
- * @s: string input
- * Return: integer value
- */
+* _atoi - Converts a string to an integer.
+* @s: The input string.
+*
+* Return: The converted integer.
+*/
 int _atoi(char *s)
 {
-int i = 0;
 int sign = 1;
-unsigned int num = 0;
-while (s[i] != '\0')
+int result = 0;
+int i = 0;
+while (s[i] && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
 sign *= -1;
-if (s[i] >= '0' && s[i] <= '9')
-{
+i++;
+}
 while (s[i] >= '0' && s[i] <= '9')
 {
-num = num * 10 + (s[i] - '0');
+if (result > (INT_MAX - (s[i] - '0')) / 10)
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
+}
+result = result * 10 + (s[i] - '0');
 i++;
 }
-break;
-}
-i++;
-}
-return (num * sign);
+return (result *sign);
 }
