c12d724bd94da98214609a7410569a11cad0f234
Your Name <you@example.com>
2026-03-06 17:44:40 +0300

    Holberton task

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -18,13 +18,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
-
-    while (s1[i] == s2[i] && s1[i] != '\0')
-    {
-        i++;
-    }
-
-    return (s1[i] - s2[i]);
+int i = 0;
+while (s1[i] == s2[i] && s1[i] != '\0')
+{
+i++;
 }
-
+return (s1[i] - s2[i]);
+}
