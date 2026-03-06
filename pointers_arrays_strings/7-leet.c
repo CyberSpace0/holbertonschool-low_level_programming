116cc16a5836d992e8519bd49586cc271a77dd94
Your Name <you@example.com>
2026-03-06 23:36:04 +0300

    Holberton task

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *s)
 {
 int i, j;
-int new_word = 1;  // بداية كلمة
+int new_word = 1;
 char letters[] = {'a','A','e','E','o','O','t','T','l','L','s','S'};
 char codes[]   = {'4','4','3','3','0','0','7','7','1','1','5','5'};
 for (i = 0; s[i] != '\0'; i++)
