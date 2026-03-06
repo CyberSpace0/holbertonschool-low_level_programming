17b8ac431384fc45f8a836b55bbb0071bb6f99e9
Your Name <you@example.com>
2026-03-06 23:45:18 +0300

    Holberton task

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,32 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * leet - Entry point
- * @s: char vlue
- * Return: alwasys (0) successed
- */
+* leet - Encodes a string into 1337.
+* @s: The input string.
+* Return: The modified string.
+*/
 char *leet(char *s)
 {
-int i, j;
-int new_word = 1;
-char letters[] = {'a','A','e','E','o','O','t','T','l','L','s','S'};
-char codes[]   = {'4','4','3','3','0','0','7','7','1','1','5','5'};
-for (i = 0; s[i] != '\0'; i++)
+char *p = s;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+int i;
+while (*p)
 {
-for (j = 0; j < 12; j++)
+for (i = 0; letters[i]; i++)
 {
-if (s[i] == letters[j] && !(new_word && (s[i]=='S' || s[i]=='L')))
+if (*p == letters[i])
 {
-s[i] = codes[j];
+*p = numbers[i];
 }
 }
-if ((s[i] >= 'A' && s[i] <= 'Z') ||
-(s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= '0' && s[i] <= '9'))
-new_word = 0;
-else
-new_word = 1;
+p++;
 }
 return (s);
 }
