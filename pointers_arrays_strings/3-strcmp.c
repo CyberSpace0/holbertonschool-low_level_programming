9f8475e15bafe077b77b46eed8e416bc509736c4
Your Name <you@example.com>
2026-03-06 17:45:05 +0300

    Holberton task

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,6 @@
  * @s2: source string
  * Return: pointer to dest
  */
-#include <stdio.h>
-#include "main.h"
-
-/**
- * _strcmp - concatenate two strings
- * @s1: destination string
- * @s2: source string
- * Return: pointer to dest
- */
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
