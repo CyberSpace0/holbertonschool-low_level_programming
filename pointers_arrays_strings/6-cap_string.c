5ac96f391db3568d8964803a41a17a5b8a050dae
Your Name <you@example.com>
2026-03-06 23:19:22 +0300

    Holberton task

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+int exceptions(char k);
+
+/**
+ * cap_string - concatenate two strings
+ * @s: destination string
+ * Return: pointer to dest
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+if (exceptions(s[i-1]))
+{
+s[i] = s[i] - 32;
+}
+}
+i++;
+}
+return (s);
+}
+
+/**
+ * exceptions - concatenate two strings
+ * @k: destination string
+ * Return: pointer to dest
+ */
+int exceptions(char k)
+{
+int i = 0;
+char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+while (i < 13)
+{
+if (k == sep[i])
+{
+return (1);
+}
+i++;
+}
+return (0);
+}
