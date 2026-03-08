f406ac940e5ea53bdfbafac9cd7a018af469b70e
root <root@ip-10-42-218-37.ec2.internal>
2026-03-08 12:08:45 +0000

    dfa

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Entry function
+ * @s: int value from pointer
+ * @c: char
+ * Return: always int value
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+int x = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+for (x = 0; x < i; x++)
+{
+s++;
+}
+return (s);
+}
+i++;
+}
+
+return (NULL);
+}
