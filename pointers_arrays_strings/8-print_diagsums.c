87d47b0d2a6afac07b50664fdfcf29a0f852e31e
Your Name <you@example.com>
2026-03-09 00:41:29 +0300

    Holberton task

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Entry function
+ * @a: int value from pointer
+ * @size: int value
+ * Return: always int value
+ */
+void print_diagsums(int *a, int size)
+{
+int i = 0;
+int result = 0;
+int num = 0;
+for (i = 0;i < size;i++)
+{
+result = result + (a[i * size + i]);
+num = num + (a[i * size + (size - 1 - i)]);
+}
+printf("%i, %i\n",result,num);
+
+}
