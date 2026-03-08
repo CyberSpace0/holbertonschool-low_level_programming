e51cf3070dcaf1f7b299343f5d537e9d4fd1c74a
Your Name <you@example.com>
2026-03-08 22:50:54 +0300

    Holberton task

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,27 +12,30 @@ unsigned int _strspn(char *s, char *accept)
 int i = 0;
 int x = 0;
 int result = 0;
-char state[] = "false";
+char state = 'f';
 while (s[x] != '\0')
 {
+i = 0;
 while (accept[i] != '\0')
 {
 if (accept[i] == s[x])
 {
-state = "true";
+state = 't';
 result++;
 break;
 }
 else if(accept[i + 1] == '\0')
 {
-state = "false";
+state = 'f';
 }
 i++;
 }
 x++;
-}
-if (state != "true")
+if (state != 't')
 {
 break;
 }
+
+}
+return (result);
 }
