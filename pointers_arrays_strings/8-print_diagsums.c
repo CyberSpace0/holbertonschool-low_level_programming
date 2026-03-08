cc5cef0323e9695151896b4aab4136f0a58fe7f5
Your Name <you@example.com>
2026-03-09 00:42:03 +0300

    Holberton task

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,11 @@ void print_diagsums(int *a, int size)
 int i = 0;
 int result = 0;
 int num = 0;
-for (i = 0;i < size;i++)
+for (i = 0; i < size; i++)
 {
 result = result + (a[i * size + i]);
 num = num + (a[i * size + (size - 1 - i)]);
 }
-printf("%i, %i\n",result,num);
+printf("%i, %i\n", result, num);
 
 }
