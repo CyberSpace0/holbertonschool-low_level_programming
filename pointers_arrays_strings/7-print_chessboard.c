f9fc99c69ccf9146c63d3c3db16397d4ce9e06b4
Your Name <you@example.com>
2026-03-09 00:12:33 +0300

    Holberton task

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,10 +8,10 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i,x;
-for (i = 0;i < 8;i++)
+int i, x;
+for (i = 0; i < 8; i++)
 {
-for (x = 0;x < 8;x++)
+for (x = 0; x < 8; x++)
 {
 _putchar(a[i][x]);
 }
