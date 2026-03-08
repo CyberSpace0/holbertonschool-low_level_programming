d99b29e860d2a3d58474e70617e7444291c7c6c4
root <root@ip-10-42-218-37.ec2.internal>
2026-03-08 11:25:57 +0000

    dd

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  * @b: char also
  * Return: always int value
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
 while (s[i] != '\0' && i < n)
