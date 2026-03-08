cad1ea86b4acaefd98e38c8101b23918cc487fdc
root <root@ip-10-42-218-37.ec2.internal>
2026-03-08 11:24:06 +0000

    dd

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - Entry function
+ * @n: int value from pointer
+ * @s: char
+ * @b: char also
+ * Return: always int value
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+int i = 0;
+while (s[i] != '\0' && i < n)
+{
+s[i] = b;
+i++;
+}
+
+}
