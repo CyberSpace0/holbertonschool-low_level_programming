8aa0db40a4385a1dcd48312bedcd3e374f6397d2
Your Name <you@example.com>
2026-03-10 02:50:41 +0300

    Holberton task

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
 * main - entry point
 * @argc: length
@@ -10,8 +11,11 @@ int main(int argc, char *argv[])
 {
 if (argc > 1)
 {
-printf("%i\n", argv[1] * argv[2]);
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 }
-
+else
+{
+printf("Error\n");
+}
 return (0);
 }
