5dc7c504173283e9c2caec823f6ff354191868ae
Your Name <you@example.com>
2026-03-09 15:08:01 +0300

    Holberton Task

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - concatenate two strings
- * @s: destination string
+ * factorial - concatenate two strings
+ * @n: destination string
  * Return: pointer to dest
  */
 int factorial(int n)
-{
-if (n <  0)
-{
-return (-1);
-}
-else
 {
 if (n != 0)
 {
 return (n * factorial(n - 1));
 }
 return (n * n + 1);
-}
 }
