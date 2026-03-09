da31b05b35d66ecbd0e4d801d3f4df0bdcac353c
Your Name <you@example.com>
2026-03-10 02:35:01 +0300

    Holberton task

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: length
+* @argv: char
+* Return: always (0) success
+*/
+int main(int argc, char *argv[])
+{
+
+printf("%i\n", argc);
+
+return (0);
+}
