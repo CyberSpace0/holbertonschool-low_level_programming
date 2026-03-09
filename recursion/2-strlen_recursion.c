7cd6214b5f932f89d508cc27dde192400ad1eebf
Your Name <you@example.com>
2026-03-09 14:40:09 +0300

    Holberton Task

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - concatenate two strings
+ * @s: destination string
+ * Return: pointer to dest
+ */
+int _strlen_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+
+}
