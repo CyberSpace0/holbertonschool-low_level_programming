0e39ade304bf782d035e5fda94444c0efff3ef4b
Your Name <you@example.com>
2026-03-09 15:26:29 +0300

    Holberton Task

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - concatenate two strings
+ * @n: destination string
+ * Return: pointer to dest
+ */
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+{
+return (-1);
+}
+if (y > 0)
+{
+return (x * _pow_recursion(x, y - 1));
+}
+return (1);
+}
