2970245b34c74c274566ef9ef8738f7361019082
Your Name <you@example.com>
2026-03-09 22:34:56 +0300

    Holberton task

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int ssqrt(int i, int x);
+
+/**
+ * _sqrt_recursion - concatenate two strings
+ * @n: destination string
+ * Return: pointer to dest
+ */
+int is_prime_number(int n)
+{
+int i = 2;
+return (ssqrt(n, i));
+
+}
+
+/**
+ * ssqrt - secondary entrpoint
+ * @n: int value
+ * @x: int value
+ * Return: always int
+ */
+int ssqrt(int n, int x)
+{
+if (n > 1)
+{
+if (n == x)
+{
+return (1);
+}
+if (n % x == 0)
+{
+return (0);
+}
+else
+{
+return (ssqrt(n, x + 1) );
+}
+}
+else
+{
+return (0);
+}
+}
