2fe86bf140c407833789123fb91c42c05a2cb026
Your Name <you@example.com>
2026-03-30 18:09:26 +0300

    daf

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* alloc_grid - entry point
+* @width: length
+* @height: length
+* Return: always (0) success
+*/
+void free_grid(int **grid, int height)
+{
+free(grid);
+}
