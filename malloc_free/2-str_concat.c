e03310f4c6e84e5773379a44880c73c17d0e97bc
MN7 <you@example.com>
2026-03-30 11:18:54 +0300

    Holberton Task

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - entry point
+* @str: length
+* Return: always (0) success
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *arr;
+unsigned int i;
+int l1,l2,s;
+for (l1 = 0; s1[l1] != '\0';l1++)
+{
+
+}
+for (l2 = 0; s2[l2] != '\0';l2++)
+{
+
+}
+
+
+if (s1 == NULL && s2 == NULL)
+{
+return ('\0');
+}
+arr = malloc((l1 + l2 + 1) * sizeof(char));
+if (arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < l1; i++)
+{
+arr[i] = s1[i];
+}
+arr[i+1] = ' ';
+for (s = 0; s < l2; s++)
+{
+arr[i + 2 + s] = s2[s];
+}
+
+return (arr);
+
+}
