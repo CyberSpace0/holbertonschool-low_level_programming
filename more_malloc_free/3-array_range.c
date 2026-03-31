7020c2a14ef8c8a296d96ac3e108a13df349ff04
MN7 <you@example.com>
2026-03-31 12:34:58 +0300

    Holberton Task

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 int* arr;
 int i;
 int x = 0;
-if (min >= max)
+if (min > max)
 {
 return (NULL);
 }
@@ -23,7 +23,7 @@ if (arr == NULL)
 {
 return (NULL);
 }
-for (i = min; i < max; i++)
+for (i = min; i <= max; i++)
 {
 arr[x] = i;
 x++; 
