fa6032727d59033e0a30f2c682c1ea840dc004b6
MN7 <you@example.com>
2026-03-31 10:50:29 +0300

    daf

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int _strlen(char *str);
+
+/**
+* malloc_checked - entry point
+* @b: length
+* Return: always (0) success
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *ptr;
+int i,x;
+if (s1 == NULL || s2 == NULL)
+{
+return ('\0');
+}
+ptr = calloc((_strlen(s1) + n), sizeof(char));
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < _strlen(s1); i++)
+{
+ptr[i] = s1[i];
+}
+for (x = 0; x < n; x++)
+{
+ptr[i + x] = s2[x];
+}
+
+return (ptr);
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to measure
+ * Return: length of the string
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+return (len);
+}
