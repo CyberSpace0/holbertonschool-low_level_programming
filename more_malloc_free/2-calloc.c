868f61fe2d298ac60c07418109871fd1d2387aad
MN7 <you@example.com>
2026-03-31 12:04:40 +0300

    Holberton Task

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,12 +12,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char *ptr = malloc(nmemb * size);
-if (ptr == NULL)
+char *ptr;
+if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-if (nmemb == 0 || size == 0)
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
 {
 return (NULL);
 }
@@ -25,22 +26,7 @@ for (i = 0; i < nmemb; i++)
 {
 ptr[i] = 0;
 }
-return (ptr);
+return ((void *)ptr);
 
 }
 
-
-/**
- * _strlen - returns the length of a string
- * @str: string to measure
- * Return: length of the string
- */
-unsigned int _strlen(char *str)
-{
-unsigned int len = 0;
-while (str[len] != '\0')
-{
-len++;
-}
-return (len);
-}
