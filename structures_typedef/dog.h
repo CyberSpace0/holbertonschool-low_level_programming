36ec25db62f2c6850c10e84f0a9e571aa8155f00
MN7 <you@example.com>
2026-04-01 13:34:59 +0300

    Holberton Task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,10 +13,9 @@ struct dog
 char *name;
 char *owner;
 float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog dog_t;
 
 
 #endif
