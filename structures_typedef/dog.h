ae43097f02e9fb7feca84a5cf4b46ddeaab30e4a
MN7 <you@example.com>
2026-04-01 12:34:25 +0300

    Holberton Task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that stores dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
