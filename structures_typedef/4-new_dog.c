a73279cf76d681999373106f0ed7198622ee9791
Your Name <you@example.com>
2026-04-04 10:46:12 +0300

    daf

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * print_dog - dsds
+ * @d: dafdf
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+int i, len_name = 0, len_owner = 0;
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
+while (name[len_name] != '\0')
+{
+len_name++;
+}
+while (owner[len_owner] != '\0')
+{
+len_owner++;
+}
+d->name = malloc(len_name + 1);
+if (d->name == NULL)
+{
+free(d);
+return (NULL);
+}
+
+d->owner = malloc(len_owner + 1);
+if (d->name == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+for (i = 0; i < len_name; i++)
+{
+d->name[i] = name[i];
+}
+d->name[i] = '\0';
+for (i = 0; i < len_owner; i++)
+{
+d->owner[i] = owner[i];
+}
+d->owner[i] = '\0';
+
+d->age = age;
+
+return (d);
+}
