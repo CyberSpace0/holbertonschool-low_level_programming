6147cb314880bdd35197579d5c4dc045fde96704
MN7 <you@example.com>
2026-04-06 12:33:16 +0300

    Holberton Task

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+int r;
+int (*ptr)(int, int);
+if (argc == 3)
+{
+printf("Error");
+exit(98);
+}
+if ((argv[2] == "%" || argv[2] == "/") && (argv[1] == 0 || argv[3] == 0))
+{
+printf("Error");
+exit(100);
+}
+ptr = get_op_func(argv[2]);
+if (ptr == 0)
+{
+printf("Error");
+exit(99);
+}
+r = ptr(atoi(argv[1]), atoi(argv[3]));
+printf("%i", r);
+
+
+return (0);
+}
