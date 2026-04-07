94879bd04b9e50e848c540aae4e5103bf58be368
MN7 <you@example.com>
2026-04-07 11:15:21 +0300

    Holberton Task

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i;
 va_start(args, n);
-char *ss;
+char* ss;
 if (separator == NULL)
 {
 separator = "";
