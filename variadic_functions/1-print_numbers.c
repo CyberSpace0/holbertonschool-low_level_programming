a0441c5948fee6180312d014a2e165ca7a5ba772
MN7 <you@example.com>
2026-04-07 10:48:54 +0300

    Holberton Task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,6 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i;
 va_start(args, n);
+if (separator == NULL)
+{
+separator = '';
+}
 for (i = 0; i < n; i++)
 {
 if (i == (n - 1))
