2462bac7290276aee765a8012e319c6efeced438
MN7 <you@example.com>
2026-04-07 10:43:55 +0300

    Holberton Task

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - check the code
+ * @separator: int value
+ * @n: int value
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+if (i == (n - 1))
+{
+printf("%i",va_arg(args, int));
+}
+else
+{
+printf("%i%s",va_arg(args, int), separator);
+}
+}
+va_end(args);
+printf('\n');
+}
--- variadic_functions/main.c
+++ variadic_functions/main.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
-
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("%i" ,sum_them_all(0,1,4,3));
+print_numbers(", ", 4, 0, 98, -1024, 402);
 return (0);
-}   
+}
