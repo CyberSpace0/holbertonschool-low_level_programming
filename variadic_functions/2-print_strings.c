9360bbda93f324961da71d352afc0722907f78ee
MN7 <you@example.com>
2026-04-07 11:14:44 +0300

    Holberton Task

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_strings - check the code
+ * @separator: int value
+ * @n: int value
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+va_start(args, n);
+char *ss;
+if (separator == NULL)
+{
+separator = "";
+}
+for (i = 0; i < n; i++)
+{
+ss = va_arg(args, char*);
+if (i == (n - 1))
+{
+printf("%s", ss);
+}
+else
+{
+
+if (ss == NULL)
+{
+ss = "nil";
+printf("%s%s", ss, separator);
+}
+else
+{
+printf("%s%s", ss, separator);
+}
+
+}
+}
+va_end(args);
+printf("\n");
+}
