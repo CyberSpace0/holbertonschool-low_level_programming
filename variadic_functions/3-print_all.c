0a54f83b88ca44e34fa5b49e5ed34d7aad05a6f2
MN7 <you@example.com>
2026-04-08 09:51:50 +0300

    Holberton Task

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int _strlen(const char *str);
+
+
+/**
+ * print_all - check the code
+ * @format: int value
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+int len, i = 0;
+char *sep;
+va_list args;
+va_start(args, format); 
+len = _strlen(format);
+while (len > i)
+{
+
+if (i == len - 1)
+{
+sep = "";
+}
+else
+{
+sep = ", ";
+}
+switch (format[i])
+{
+case 's':
+printf("%s%s", va_arg(args, char*), sep);
+break;
+case 'c':
+printf("%c%s", va_arg(args, int), sep);
+break;
+case 'i':
+printf("%i%s", va_arg(args, int), sep);
+break;
+case 'f':
+printf("%f%s", va_arg(args, double), sep);
+break;
+
+
+}
+i++;
+}
+va_end(args);
+
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to measure
+ * Return: length of the string
+ */
+int _strlen(const char *str)
+{
+unsigned int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+return (len);
+}
+
