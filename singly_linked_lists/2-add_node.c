e66ba1a69dd3a5598c062d572ccd16d1d9888fd5
MN7 <you@example.com>
2026-04-21 14:26:19 +0300

    Holberton Task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *node;
 node = malloc(sizeof(list_t));
+if (node == NULL)
+return (NULL);
 node->str = strdup(str);
+if (node->str == NULL)
+{
+free(node);
+return (NULL);
+}
+
 node->len = 0;
 node->next = *head;
 
