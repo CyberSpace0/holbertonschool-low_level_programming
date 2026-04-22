5225abecc545925e685e2af7a5da5258df6d91cf
Your Name <you@example.com>
2026-04-22 17:37:23 +0300

    Holberton task

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - check the code
+ * @head:dd
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+
+list_t *node;
+while (head != NULL)
+{
+node = head->next;
+
+free(head->n);
+free(head->prev);
+free(head);
+
+head = node;
+}
+
+}
