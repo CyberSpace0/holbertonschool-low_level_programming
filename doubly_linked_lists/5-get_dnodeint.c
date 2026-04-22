6a24b2ae0ec4f69724432174d1cb42249dc133c6
Your Name <you@example.com>
2026-04-22 17:57:02 +0300

    Holberton task

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 unsigned int i = 0;
 dlistint_t *node;
 node = head;
-if (index < 0)
+if (index == NULL)
 {
 return (NULL);
 }
