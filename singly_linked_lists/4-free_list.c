489e4487e147dbf7839547bde583f91e4b3745d9
MN7 <you@example.com>
2026-04-22 11:45:55 +0300

    Holberton Task

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <unistd.h>
+
+
+/**
+ * free_list - check the code
+ * @head: ddd
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+while (*head != NULL)
+{
+free(*head->str);
+free(*head->len);
+free(*head->next);
+
+}
+
+}
+
