fa8fd9922b05d247733c2763c244405c44c3e486
MN7 <you@example.com>
2026-04-26 14:39:21 +0300

    Holberton Task

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,46 +12,41 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *node = *head;
+dlistint_t *tmp;
 unsigned int i = 0;
-
 if (head == NULL || *head == NULL)
 {
 return (-1);
 }
-
 if (index == 0)
 {
-node = node->next;
-free(node->prev);
-node->prev = NULL;
-*head = node;
+tmp = node;
+*head = node->next;
+if (*head != NULL)
+{
+(*head)->prev = NULL;
+}
+free(tmp);
 return (1);
 }
-
-while (node != NULL && i < index - 1)
+while (node != NULL && i < index)
 {
 node = node->next;
 i++;
 }
-
 if (node == NULL)
 {
 return (-1);
 }
-
-if (node->next->next != NULL)
+tmp = node;
+if (node->prev)
 {
-node->next = node->next->next;
-free(node->next->prev);
-node->next->prev = node;
-return (1);
+node->prev->next = node->next;
 }
-else
+if (node->next)
 {
-node->next = NULL;
+node->next->prev = node->prev;
 }
-
-
-free(node);
+free(tmp);
 return (1);
 }
