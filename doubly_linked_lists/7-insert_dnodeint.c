d9683be690dd49ec0b665c11423fab9d0e967a28
MN7 <you@example.com>
2026-04-26 11:45:00 +0300

    Holberton Task

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,51 +12,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int i = 0;
 dlistint_t *node = *h;
-dlistint_t *ls = malloc(sizeof(dlistint_t)); 
+dlistint_t *ls = malloc(sizeof(dlistint_t));
 if (ls == NULL)
 {
 return (NULL);
 }
 ls->n = n;
 
-
 if (idx == 0)
 {
 ls->prev = NULL;
 ls->next = *h;
 if (*h != NULL)
-{
-(*h)->prev = ls;
-}
+    (*h)->prev = ls;
 *h = ls;
 return (ls);
 }
-
-
 while (node != NULL && i < idx)
-{
-if (i == idx)
-{
-ls->next = node;
-ls->prev = node->prev;
-if (node->prev != NULL)
-{
-node->prev->next = ls;
-}
-else
-{
-*h = ls;
-}
-node->prev = ls;
-break;
-}
-else
 {
 node = node->next;
 i++;
 }
-}
-
+if (node == NULL)
+    return (NULL);
+ls->next = node;
+ls->prev = node->prev;
+node->prev->next = ls;
+node->prev = ls;
 return (ls);
-
 }
