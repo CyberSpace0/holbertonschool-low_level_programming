43ccaa76ce9ac5526b5bcd33c52e35a007e646c7
MN7 <you@example.com>
2026-04-26 11:22:06 +0300

    Holberton Task

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - check the code
+ * @head:dd
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+unsigned int i = 0,total = 0;
+dlistint_t *node = *h;
+dlistint_t *ls = malloc(sizeof(dlistint_t)); 
+if (ls == NULL)
+{
+return (NULL);
+}
+ls->n = n;
+
+
+if (h == NULL)
+{
+return (NULL);
+}
+
+while (node != NULL)
+{
+if (i == idx)
+{
+ls->next = node->next;
+ls->prev = node;
+node->next = ls;
+i++;
+}
+else
+{
+node = node->next;
+i++;
+}
+}
+
+return (node);
+
+}
