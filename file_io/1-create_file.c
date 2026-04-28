06de24fe03a895b5ad08fe11ef71eeae9dc2daf1
MN7 <you@example.com>
2026-04-28 13:18:24 +0300

    Holberton Task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,8 @@
 int create_file(const char *filename, char *text_content)
 {
 int i = 0;
-int fp = open(filename, O_WRONLY | O_CREAT, 0600);
+char x[100];
+int fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (filename == NULL)
 {
 return (-1);
@@ -27,7 +28,6 @@ while (text_content[i] != '\0')
 {
 i++;
 }
-
 
 write(fp, text_content, i);
 
