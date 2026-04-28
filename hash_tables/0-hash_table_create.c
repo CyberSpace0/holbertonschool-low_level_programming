a380692e4dbe776fa89c04ef9bb22ee825ad0b12
Your Name <you@example.com>
2026-04-28 18:59:36 +0300

    Holberton task

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* main - entry point
+* Return: always (0) success
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *table;
+
+table = malloc(sizeof(hash_table_t));
+
+return (table);
+}
