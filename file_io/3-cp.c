7486e7acd8cf10d12afee999f9a96a48ebfc0cca
MN7 <you@example.com>
2026-04-28 15:27:47 +0300

    Holberton Task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,10 +6,13 @@
 #define _POSIX_C_SOURCE 200809L
 #include "main.h"
 
+void print_error_read(char *file, int len);
+void print_error_write(char *file, int len);
+
 /**
  * main - check the code
- * @argc: file
- * @argv: chars
+ * @argc: args count
+ * @argv: args vector
  * Return: Always EXIT_SUCCESS.
  */
 int main(int argc, char *argv[])
@@ -28,50 +31,36 @@ exit(97);
 }
 
 while (argv[1][farg] != '\0')
-{
 farg++;
-}
 
 while (argv[2][sarg] != '\0')
-{
 sarg++;
-}
 
 from = open(argv[1], O_RDONLY, 0664);
 if (from < 0)
 {
-write(STDERR_FILENO, "Error: Can't read from file ", 28);
-for (i = 0; i < farg; i++)
-{
-_putchar(argv[1][i]);
-}
-_putchar('\n');
+print_error_read(argv[1], farg);
 exit(98);
 }
 
 data = read(from, info, 3000);
 if (data < 0)
 {
-write(STDERR_FILENO, "Error: Can't read from file ", 28);
-for (i = 0; i < farg; i++)
-{
-_putchar(argv[1][i]);
-}
-_putchar('\n');
+print_error_read(argv[1], farg);
 exit(98);
 }
 
 to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 if (to < 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+print_error_write(argv[2], sarg);
 exit(99);
 }
 
 error = write(to, info, data);
 if (error == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+print_error_write(argv[2], sarg);
 exit(99);
 }
 
@@ -90,4 +79,30 @@ exit(100);
 }
 
 return (0);
+}
+
+/**
+ * print_error_read - print read error
+ */
+void print_error_read(char *file, int len)
+{
+int i;
+
+write(STDERR_FILENO, "Error: Can't read from file ", 28);
+for (i = 0; i < len; i++)
+_putchar(file[i]);
+_putchar('\n');
+}
+
+/**
+ * print_error_write - print write error
+ */
+void print_error_write(char *file, int len)
+{
+int i;
+
+write(STDERR_FILENO, "Error: Can't write to ", 22);
+for (i = 0; i < len; i++)
+_putchar(file[i]);
+_putchar('\n');
 }
