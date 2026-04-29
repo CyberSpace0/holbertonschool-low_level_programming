030380a59ee2aa323602d0556727fcb38dce7ed6
MN7 <you@example.com>
2026-04-29 11:56:24 +0300

    Holberton Task

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,19 +14,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 long int index;
-hash_node_t *node = malloc(sizeof(hash_node_t *));
+hash_node_t *node = malloc(sizeof(hash_node_t));
 if (node == NULL)
 {
 return (0);
 }
+
 node->key = strdup(key);
 node->value = strdup(value);
-node->next = NULL;
 
-index = hash_djb2(key) % 10;
-ht->size = 10;
+if (node->key == NULL || node->value == NULL)
+{
+return (0);
+}
+
+node->next = ht->array[index];
+
+index = key_index(key, ht->size);
 ht->array[index] = node;
-
 
 return (1);
 }
