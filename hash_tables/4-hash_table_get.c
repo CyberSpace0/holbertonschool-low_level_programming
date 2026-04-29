54cbd2ee6367828b5cf4d0f521a5a6d760d86f3f
MN7 <you@example.com>
2026-04-29 18:10:29 +0300

    Holberton Task

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* hash_table_set - entry point
+* @ht: string to generate the real key for dictionary
+* @key: size of array
+* @value: the value
+* Return: always (0) success
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+
+index = key_index(key, ht->size);
+
+return (ht->array[index]->value);
+}
