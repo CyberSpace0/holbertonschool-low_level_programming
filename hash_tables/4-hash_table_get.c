8c38793e20830622e5259d2ea18601630104b448
MN7 <you@example.com>
2026-04-30 01:59:27 +0300

    Holberton Task

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,8 +14,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int index;
-
-index = (unsigned long int)key % ht->size;
-
-return (ht->array[index]->value);
+hash_node_t *tmp;
+if (ht == NULL || key == NULL || *key == '\0')
+{
+return (NULL);
+}
+index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+{
+return (tmp->value);
+}
+tmp = tmp->next;
+}
+return (NULL);
 }
