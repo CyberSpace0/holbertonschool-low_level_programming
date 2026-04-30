5ea4badda9532dd26886c5f0c3eb94b200c31d65
MN7 <you@example.com>
2026-04-30 13:38:58 +0300

    Holberton Task

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* hash_table_get - entry point
+* @ht: string to generate the real key for dictionary
+* @key: size of array
+* Return: always (0) success
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+long unsigned int i;
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i]->key != NULL)
+{
+printf("'%s': '%s'", ht->array[i]->key,ht->array[i]->value);
+}
+printf("--");
+}
+
+}
