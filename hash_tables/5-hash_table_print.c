43c09c1c57fb0f0754842b80249eae73206affac
MN7 <you@example.com>
2026-04-30 14:56:44 +0300

    Holberton Task

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -15,28 +15,25 @@ void hash_table_print(const hash_table_t *ht)
 hash_node_t *node;
 long unsigned int i = 0;
 int first = 1;
-node = ht->array[i];
 printf("{");
 if (ht != NULL)
 {
 for (i = 0; i < ht->size; i++)
 {
+node = ht->array[i];
 if (node != NULL)
 {
 if (!first)
 {
 printf(", ");
 }
-if (node->next != NULL)
+printf("'%s': '%s'", node->key, node->value);
+while (node->next != NULL)
 {
 node = node->next;
-if (!first)
-{
-printf(", ");
-}
 printf("'%s': '%s'", node->key, node->value);
+
 }
-printf("'%s': '%s'", node->key, node->value);
 first = 0;
 }
 }
