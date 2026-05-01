3c7288e8864e34a2a576b5b76ac125f472c12531
MN7 <you@example.com>
2026-05-01 16:51:18 +0300

    Holberton Task

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,3 +1,16 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+* hash_table_set - entry point
+* @ht: string to generate the real key for dictionary
+* @key: size of array
+* @value: the value
+* Return: always (0) success
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
