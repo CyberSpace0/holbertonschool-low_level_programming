c3a16ae7f2639c47bbb02728593e431519c19ce8
MN7 <you@example.com>
2026-05-01 16:11:43 +0300

    Holberton Task

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -14,9 +14,9 @@ void hash_table_delete(hash_table_t *ht)
 hash_node_t *node;
 unsigned long int i = 0;
 
-node = *ht->array;
 for (i = 0; i < ht->size; i++)
 {
+node = ht->array[i];
 free(node[i]->key);
 free(node[i]->value);
 free(node[i]);
